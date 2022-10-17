158eb8599073b4348be6ce0965c5461e0866feb6
Beatrice <ushiksmandi@gmail.com>
2022-10-17 09:19:16 -0700

    printf

--- _putchar.c
+++ _putchar.c
@@ -4,6 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: the character to print
  * Return: On success 0
+ * On error, -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)
